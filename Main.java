af5517a61006d99795e4608619b9b2d8c4faf75b
ryancowger <46778514+ryancowger@users.noreply.github.com>
2019-07-22 13:51:28 -0500

    Add files via upload
    I added these three main files showing you some more basic main files.

--- Main.java
+++ Main.java
@@ -1,43 +1,28 @@
-import java.time.*;
-
-
+package com.ryancowger;
 
 public class Main {
 
+    public static void main(String[] args) {
+	     String myString = "This is a String";
+
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is  " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("Last is Eqaul to " + lastString);
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString Value: " + lastString);
 
 
-public static void main(String[] args) {
-
-
-
-LocalDate date = LocalDate.now();
-
-
-
-int currentDate = date.getYear();
-
-
-
-if (currentDate > 1980) {
-
-
-
-System.out.println("Today is later than January 1, 1980.");
-
-
-
-}
-
-if (currentDate < 2020) {
-
-
-
-System.out.println("Today is earlier than January 1, 2020.");
-
-
-
-}
-
-}
-
+    }
 }
